ab2052dd5c967e2ebcae3bee9fb15e67328c231c
dima <brilliantcontract@yahoo.com>
2025-07-15 18:29:31 +0300

    Disable supplier options without script
    Added logic in bulk-supplier-check.html to mark options with empty script as disabled. Updated BulkSupplierCheckHtmlTest to ensure this code is present.

--- src/test/java/bc/mro/mrosupply_com_api_caller/BulkSupplierCheckHtmlTest.java
+++ src/test/java/bc/mro/mrosupply_com_api_caller/BulkSupplierCheckHtmlTest.java
@@ -18,4 +18,10 @@ public class BulkSupplierCheckHtmlTest {
         assertThat(html, containsString("Bulk check"));
         assertThat(html, containsString("Copy available"));
     }
+
+    @Test
+    public void optionDisabledWhenScriptEmpty() throws Exception {
+        String html = new String(Files.readAllBytes(Paths.get("src/main/webapp/bulk-supplier-check.html")), StandardCharsets.UTF_8);
+        assertThat(html, containsString("o.disabled = true;"));
+    }
 }
